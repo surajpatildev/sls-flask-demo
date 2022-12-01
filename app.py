c50ab5aa14e5ae2110c501f17bad5d44cab771a6
Suraj Patil <surajtammapatil@gmail.com>
2022-12-01 18:14:24 +0530

    minor

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_from_root():
-    event = request.environ.get('serverless.event')
+    event = request.environ.get('serverless.event', {})
     print("event", event)
     claims = event.get("authorizer", {}).get("claims", {})
 
