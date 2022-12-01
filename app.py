4554d99a13121f5baed44300ad0ec913386a92b6
Suraj Patil <surajtammapatil@gmail.com>
2022-12-01 18:03:10 +0530

    minor fixes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, make_response, request
+from boto3 import client
 
 app = Flask(__name__)
 
@@ -8,8 +9,14 @@ def hello_from_root():
     event = request.environ.get('serverless.event')
     print("event", event)
     claims = event.get("authorizer", {}).get("claims", {})
+
+    conn = client('s3')  # again assumes boto.cfg setup, assume AWS S3
+    keys = []
+    for key in conn.list_objects(Bucket='notesaver-backend').get("Contents", []):
+        print('bucket', key['Key'])
+        keys.append(key['Key'])
     print("claims", claims)
-    return jsonify(message='Hello from root!', claims=claims)
+    return jsonify(message='Hello from root!', keys=keys)
 
 
 @app.errorhandler(404)
