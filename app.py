44174f18ae33ee48e22917e1a6976ca8a38bbe19
Suraj Patil <surajtammapatil@gmail.com>
2022-12-06 14:11:05 +0530

    context middleware added

--- app.py
+++ app.py
@@ -1,22 +1,51 @@
+import logging
+import os
+
 from flask import Flask, jsonify, make_response, request
-from boto3 import client
 
 app = Flask(__name__)
 
+LOGGER = logging.getLogger(__name__)
+
+
+@app.before_request
+def set_logged_in_service_context():  # pylint:disable=inconsistent-return-statements
+    is_offline = os.environ["IS_OFFLINE"]
+    if is_offline:
+        # If offline, set local user context
+        request.service = "local"
+        request.scopes = ["*"]
+    else:
+        # if via API gateway, use cognito to define scopes and service definitions
+        context = request.environ.get("serverless.context")
+        event = request.environ.get("serverless.event", {})
+
+        claims = event.get("authorizer", {}).get("claims", {})
+        client_id = claims.get("client_id")
+        scope = claims.get("scope")
+        if not all([client_id, scope]):
+            return make_response(jsonify(error="Access Denied!"), 403)
+
+        # setup context & event on request
+        request.context = context
+        request.event = event
+
+        # strip scopes to pick schema names
+        app_name = app.config["APP_NAME"]
+        scopes = scope.replace(f"{app_name}/").split(" ")
+        request.service = client_id
+        request.scopes = scopes
+
+    print(
+        f"request context set: service - {request.service} & scopes - {request.scopes}"
+    )
+
 
 @app.route("/")
-def hello_from_root():
-    event = request.environ.get('serverless.event', {})
-    print("event", event)
-    claims = event.get("authorizer", {}).get("claims", {})
-
-    conn = client('s3')  # again assumes boto.cfg setup, assume AWS S3
-    keys = []
-    for key in conn.list_objects(Bucket='notesaver-backend').get("Contents", []):
-        print('bucket', key['Key'])
-        keys.append(key['Key'])
-    print("claims", claims)
-    return jsonify(message='Hello from root!', keys=keys)
+def home():
+    print(f"v1.0: Service : {request.service}, Scopes: {request.scopes}")
+    LOGGER.info("v1.0: Service : %s, Scopes: %s", request.service, request.scopes)
+    return jsonify(message='Hello from root!')
 
 
 @app.errorhandler(404)
